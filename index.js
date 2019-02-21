d8da143edaa66439a1a77dac4a447e6b3361930c
Dione Betanin <dione.silva@agrotis.com>
2019-02-21 09:19:15 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const logRequest = (req, res) => {
+    console.info(`${req.method} ${req.originalUrl}`);
+    console.info(JSON.stringify(req.body));
+    res.status(200).send("Ok");
+};
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+app.post("*", logRequest);
+
+app.patch("*", logRequest);
+
+app.post("*", logRequest);
+
+const port = process.env.APP_PORT || process.env.PORT || 3000;
+
+app.listen(port, () => console.log("Logger running on port.", port));
